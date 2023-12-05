50c4e0726f6153384dd7e0b86ed23114fc7db108
Marco Karpf <marco.karpf@swisscom.com>
2023-12-05 21:28:09 +0100

    fix(channel) remove unused channel repo

--- src/main/java/ch/bbw/pr/sospri/channel/ChannelRepository.java
+++
@@ -1,7 +0,0 @@
-package ch.bbw.pr.sospri.channel;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ChannelRepository extends CrudRepository<Channel, Long> {
-    Channel findDistinctByTopic(String topic);
-}
